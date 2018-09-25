7fe0722e5af78dc1cfa5550426ef1e10ae84d90d
rimjhimgolf <rimjhimgolf@gmail.com>
2018-09-25 18:44:19 +0530

    1st Commit

---
+++ Android1.java
@@ -0,0 +1,10 @@
+class Android1
+{
+ public static void main(String args[])
+  {
+    System.out.println("Hello World");
+   System.out.println("Hello Acm");
+System.out.println("Hello Acm2");
+
+  }
+}
